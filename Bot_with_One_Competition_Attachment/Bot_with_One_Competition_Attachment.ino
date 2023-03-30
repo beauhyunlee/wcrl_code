7fe7a859dbb5d193ad2b1563216ad8a75e18a846
Beau Lee <blee124@binghamton.edu>
2023-03-29 21:15:58 -0400

    edited program to include future motors and blade, however most values still need to be implemented

--- Bot_with_One_Competition_Attachment/Bot_with_One_Competition_Attachment.ino
+++ Bot_with_One_Competition_Attachment/Bot_with_One_Competition_Attachment.ino
@@ -19,24 +19,48 @@ Servo ESC;          // Create servo object to control the ESC
 
 #include <SparkFun_TB6612.h> // Motor Driver Library provided by Sparkfun
 
-//Left Wheel is Motor A on the PCB
-#define L_Wheel_in1 A5  //A4
-#define L_Wheel_in2 A4  //A5
-#define L_Wheel_PWM 11  //D11
+//Front Left Wheel is Motor A on the PCB
+#define L_Front_Wheel_in1 A5  //A4
+#define L_Front_Wheel_in2 A4  //A5
+#define L_Front_Wheel_PWM 11  //D11
 
-//Left Wheel is Motor B on the PCB
-#define R_Wheel_in1 4   //D4
-#define R_Wheel_in2 8   //D8
-#define R_Wheel_PWM 10  //D10
+//Front Right Wheel is Motor B on the PCB
+#define R_Front_Wheel_in1 4   //D4
+#define R_Front_Wheel_in2 8   //D8
+#define R_Front_Wheel_PWM 10  //D10
+
+//Back Left Wheel is Motor C on the PCB
+#define L_Back_Wheel_in1   //
+#define L_Back_Wheel_in2   //
+#define L_Bacl_Wheel_PWM   //
+
+//Back Right Wheel is Motor D on the PCB
+#define R_Back_Wheel_in1    //
+#define R_Back_Wheel_in2    //
+#define R_Back_Wheel_PWM    //
+
+//Blade is Motor _ on the PCB
+#define blade_in1   //
+#define blade_in2   //
+#define blade_PWM   //
+
 
 const int motor_offset = -1;
 #define STBY 2
-Motor L_Wheel(L_Wheel_in1, L_Wheel_in2, L_Wheel_PWM, motor_offset, STBY);
-Motor R_Wheel(R_Wheel_in1, R_Wheel_in2, R_Wheel_PWM, motor_offset, STBY);
+//Front Wheels
+Motor L_Front_Wheel(L_Front_Wheel_in1, L_Front_Wheel_in2, L_Front_Wheel_PWM, motor_offset, STBY);
+Motor R_Front_Wheel(R_Front_Wheel_in1, R_Front_Wheel_in2, R_Front_Wheel_PWM, motor_offset, STBY);
+//Back Wheels
+Motor L_Back_Wheel(L_Back_Wheel_in1, L_Back_Wheel_in2, L_Back_Wheel_PWM, motor_offset, STBY);
+Motor R_Back_Wheel(R_Back_Wheel_in1, R_Back_Wheel_in2, R_Back_Wheel_PWM, motor_offset, STBY);
+//Weapon
+Motor blade(blade_in1, blade_in2, blade_PWM, moto_offset, STBY);
 
 #define CUSTOM_SETTINGS
 #define INCLUDE_GAMEPAD_MODULE
 
+//Blade start speed 
+int blade_speed = 35;
 
 void setup() {
 
@@ -52,37 +76,62 @@ void loop() {
   //Motion Control
   if (GamePad.isUpPressed())  //Move forward
   {
-    L_Wheel.drive(255);
-    R_Wheel.drive(-255);
+    // L_Front_Wheel.drive(255);
+    // R_Front_Wheel.drive(-255);
+    // L_Back_Wheel.drive(255);
+    // R_Back_Wheel.drive(-255)
+    L_Front_Wheel.drive(255);
+    R_Front_Wheel.drive(255);
+    L_Back_Wheel.drive(255);
+    R_Back_Wheel.drive(255);
   }
   else if (GamePad.isDownPressed()) //Move backward
   {
-    L_Wheel.drive(-255);
-    R_Wheel.drive(255);
+    // L_Front_Wheel.drive(-255);
+    // R_Front_Wheel.drive(255);
+    L_Front_Wheel.drive(-255);
+    R_Front_Wheel.drive(-255);
+    L_Back_Wheel.drive(-255);
+    R_Back_Wheel.drive(-255);
   }
   else if (GamePad.isLeftPressed()) { //Move left
-    L_Wheel.drive(-255);
-    R_Wheel.drive(-255);
+    // L_Front_Wheel.drive(-255);
+    // R_Front_Wheel.drive(-255);
+    L_Front_Wheel.drive(-255);
+    R_Front_Wheel.drive(255);
+    L_Back_Wheel.drive(-255);
+    R_Back_Wheel.drive(255);
+
   }
   else if (GamePad.isRightPressed()) { //Move right
-    L_Wheel.drive(255);
-    R_Wheel.drive(255);
+    L_Front_Wheel.drive(255);
+    R_Front_Wheel.drive(-255);
+    L_Back_Wheel.drive(255);
+    R_Back_Wheel.drive(-255);
   }
   else {  //Idle
-    L_Wheel.drive(0);
-    R_Wheel.drive(0);
+    L_Front_Wheel.drive(0);
+    R_Front_Wheel.drive(0);
+    L_Back_Wheel.drive(0);
+    R_Back_Wheel.drive(0);
   }
 
   //Competition Attachment (Weapon) Control
   if (GamePad.isTrianglePressed())  //Start up the Competition attachment
   {
-    ESC.write(35);
+    ESC.write(blade_speed); // speed of weapon
     Serial.print("Triangle");
   }
+
+
   if (GamePad.isCirclePressed())  //Increase the speed of the competition attachment
   {
-    ESC.write(60);
+    blade_speed = blade_speed + 10;
+    ESC.write(blade_speed);
     Serial.print("Circle");
+    
+    
+
   }
   if (GamePad.isCrossPressed()) //Shut off the competition attachment
   {
@@ -92,7 +141,6 @@ void loop() {
 
   if (GamePad.isStartPressed()) //Idle
   {
-
     Serial.print("Start");
     delay(100);
   }
